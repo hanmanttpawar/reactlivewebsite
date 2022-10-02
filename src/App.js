487d2570872e0a749e505c870bbd917c89fc7d8e
Hanmant Pawar <103906859+hanmanttpawar@users.noreply.github.com>
2022-10-02 05:55:42 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+
 // app () -this is commponent and jsx return
+
 function App() {
   return (
     <div className="App"> {/* class name object property*/}
